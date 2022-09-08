cb98e5e47aa8242c0b27c8c12d4e7a9239097551
Damian Garstecki <damian@garstecki.dev>
2022-09-08 13:58:31 +0200

    Controller

---
+++ src/main/java/dev/garstecki/gcp/GcpController.java
@@ -0,0 +1,12 @@
+package dev.garstecki.gcp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class GcpController {
+    @GetMapping("/")
+    String main() {
+        return "GCP V1";
+    }
+}
