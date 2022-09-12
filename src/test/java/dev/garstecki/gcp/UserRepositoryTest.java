255c5006a759f06b6c08cb40d78f816efe2150a1
Damian Garstecki <damian@garstecki.dev>
2022-09-12 07:52:13 +0200

    user repository test

---
+++ src/test/java/dev/garstecki/gcp/UserRepositoryTest.java
@@ -0,0 +1,22 @@
+package dev.garstecki.gcp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    void can_save_and_load_user() {
+        repository.save(new User("test"));
+
+        User user = repository.findAll().iterator().next();
+
+        Assertions.assertNotNull(user.id);
+        Assertions.assertEquals("test", user.name);
+    }
+}
