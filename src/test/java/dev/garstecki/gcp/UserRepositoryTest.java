2d88b9712d0440d4feb7c8d62d3c7d5b6ea63994
Damian Garstecki <damian@garstecki.dev>
2022-09-13 09:07:56 +0200

    datastore native api test

--- src/test/java/dev/garstecki/gcp/UserRepositoryTest.java
+++ src/test/java/dev/garstecki/gcp/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package dev.garstecki.gcp;
 
+import com.google.cloud.datastore.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private Datastore datastore;
+
     @Test
     void can_save_and_load_user() {
         repository.save(new User("test"));
@@ -18,5 +22,25 @@ class UserRepositoryTest {
 
         Assertions.assertNotNull(user.id);
         Assertions.assertEquals("test", user.name);
+    }
+
+    @Test
+    void use_native_api() {
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task");
+
+        var taskKey1 = datastore.allocateId(keyFactory.newKey());
+        var taskKey2 = datastore.allocateId(keyFactory.newKey());
+
+        Entity task1 = Entity.newBuilder(taskKey1).set("name", "Damian").set("smt", "ZZZ").build();
+
+        Entity task2 = Entity.newBuilder(taskKey2).set("name", 123).set("smt", "ZZZ").build();
+
+        datastore.put(task1, task2);
+
+        Query<Entity> query = Query.newEntityQueryBuilder().setKind("Task").setFilter(StructuredQuery.CompositeFilter.and(StructuredQuery.PropertyFilter.eq("name", "Damian"))).build();
+
+        QueryResults<Entity> tasks = datastore.run(query);
+
+        Assertions.assertEquals("Damian", tasks.next().getString("name"));
     }
 }
